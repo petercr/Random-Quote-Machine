fd53848d252268600287117034b06f06d4cc1ce6
Peter Cruckshank <petecruckshank@gmail.com>
2018-06-26 16:30:53 -0400

    moved getQuote from onLoad to onClick. Still getting string passed in !function

--- random-quote/src/App.js
+++ random-quote/src/App.js
@@ -14,14 +14,17 @@ class App extends Component {
 
   getQuote() {
     const url =
-      "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+      `https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?`;
     //get data from this url
+    console.log(url);
+
     fetch(url)
       .then(response => response.json())
       .then(function(data) {
         console.table(data);
       })
       .catch(error => console.error(error));
+
     // $.getJSON(url, function(data) {
     //     randomVar = data.quoteText;
     //     //passing value to corresponding div
@@ -30,15 +33,17 @@ class App extends Component {
     //     randomVar = data.quoteText + '  -' + data.quoteAuthor;
     // });
   }
+
+
   render() {
     return (
-      <div id="quote-box" onLoad="this.props.getQuote">
+      <div id="quote-box" >
         <div className="top-section">
           <p id="text">Some cool quote...</p>
           <p id="author">The one who thought it up..?!</p>
         </div>
         <div className="bottom-section">
-          <button id="new-quote">New Quote</button>
+          <button id="new-quote" onClick="this.getQuote">New Quote</button>
           <button id="tweet-quote">Tweet this quote!!</button>
         </div>
       </div>
